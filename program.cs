57856ab2977888fb4bb106c776e7ba99b0a0f689
Bryan Sambai <sambai1@live.com>
2026-09-09 18:03:08 +0200

    små justeringar

--- program.cs
+++ program.cs
@@ -1,4 +1,5 @@
-Console.WriteLine("Age total summan av notan");
+// Split the nota
+Console.WriteLine("Ange notans totala belopp");
 int summa = int.Parse(Console.ReadLine());
 Console.WriteLine("Hur många vänner är ni?");
 int antalVänner = int.Parse(Console.ReadLine());
@@ -6,5 +7,5 @@ Console.WriteLine("Ange dricksen i decimalform");
 double dricks = double.Parse(Console.ReadLine());
 double total = summa + (summa * dricks);
 double perPerson = total / antalVänner;
-Console.WriteLine("Varje person ska betala: " + perPerson);
+Console.WriteLine("Varje person ska betala: " + perPerson + "kr");
 
